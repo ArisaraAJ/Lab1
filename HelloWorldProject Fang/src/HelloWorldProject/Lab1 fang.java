a4efb6fcc947a101328221685ae39642bda1a37a
Khawfang <Khawfang@DESKTOP-8H9N86H>
2022-02-01 23:30:13 +0700

    hello world

---
+++ HelloWorldProject Fang/src/HelloWorldProject/Lab1 fang.java
@@ -0,0 +1,14 @@
+package HelloWorldProject;
+
+public class main {
+
+	public main() {
+		//end function
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}//end function
+
+}//end class
